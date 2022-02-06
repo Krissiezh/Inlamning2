d379d5e4c68ee3769ec27fd9154299a6c212862a
Krissiezh <97634398+Krissiezh@users.noreply.github.com>
2022-02-06 19:38:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//** source from https://www.youtube.com/watch?v=42M9esAvAEU&t=242s ** svårt att komma på något javascript//
+
+var typed = new typed(".auto-input",
+{ strings: ["UX-designer", "Digital Analyst", "Digital nomad"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop:true,
+})
+ /* Jag väljer ändra texter "UX-desinger, digital analyst och digtal nomad" */
